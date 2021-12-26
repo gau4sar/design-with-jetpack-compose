8e700127ae5fa775e58cae304d421e64b56c03a5
Gaurav Sarma <gaurav4sarma@gmail.com>
2021-12-27 03:54:16 +0530

    Playing with Rows, Columns, Arrangements and Alignments

--- app/src/main/java/com/gaurav/design_wtih_kotlin_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gaurav/design_wtih_kotlin_jetpackcompose/MainActivity.kt
@@ -3,10 +3,15 @@ package com.gaurav.design_wtih_kotlin_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.gaurav.design_wtih_kotlin_jetpackcompose.ui.theme.DesignwtihkotlinJetpackComposeTheme
 
@@ -14,25 +19,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DesignwtihkotlinJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            myFirstComposeView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun myFirstComposeView(){
+    DesignwtihkotlinJetpackComposeTheme {
+        Column(
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxSize(0.5f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text("Hello !! ")
+            Text("World")
+            Text("Testing")
+            Text("SpaceAround")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    DesignwtihkotlinJetpackComposeTheme {
-        Greeting("Android")
-    }
+    myFirstComposeView()
 }
