0746b972c5477716cd34fb7459271cada10f53a9
Gaurav Sarma <gaurav4sarma@gmail.com>
2021-12-28 19:03:17 +0530

    Playing with different modifiers for compose

--- app/src/main/java/com/gaurav/design_wtih_kotlin_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gaurav/design_wtih_kotlin_jetpackcompose/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gaurav.design_wtih_kotlin_jetpackcompose.ui.theme.DesignwtihkotlinJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,17 +24,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * - width/height  (if the set width exceeds the parent_content area then it resets it to the max_parent)
+ * - requiredWidth / requiredHeight (This is always equal to the defined DP value)
+ * - padding
+ * - offset
+ * - Spacer
+ */
+
 @Composable
-fun myFirstComposeView(){
+fun myFirstComposeView() {
     DesignwtihkotlinJetpackComposeTheme {
         Column(
             modifier = Modifier
                 .background(Color.Green)
-                .fillMaxSize(0.5f),
+                //.fillMaxSize(0.5f)
+                .width(320.dp)
+                .requiredHeight(150.dp)
+                .padding(10.dp)
+                .offset(15.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            Text("Hello !! ")
+            Text("Hello !! ", modifier = Modifier.clickable {
+                // New onClickListener implementation
+            })
+            Spacer(Modifier
+                .offset(20.dp, 25.dp)
+            )
             Text("World")
             Text("Testing")
             Text("SpaceAround")
